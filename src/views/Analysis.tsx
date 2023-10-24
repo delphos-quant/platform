8c62b7894168bb7051890927e7b2ca662b542c60
Rafael Zimmer <rzimmerdev@gmail.com>
2023-10-24 19:25:11 -0300

    Added analysis page

--- src/views/Analysis.tsx
+++ src/views/Analysis.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Analysis extends React.Component {
     render() {
-        return <div>Home Component</div>;
+        return (
+            <div style={{ width: '100%', height: 'calc(100vh - 193px)' }}>
+                <iframe
+                    src="series-temporais/output.html"
+                    style={{ width: '100%', height: '100%' }}
+                    title="Embedded HTML"
+                />
+            </div>
+        );
     }
 }
 
